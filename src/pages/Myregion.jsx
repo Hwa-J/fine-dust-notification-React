97e79c211c7dd499e6732174382a8dac3ab2f0c9
Hwa-J <hj1103.code@gmail.com>
2022-08-23 04:42:30 +0900

    refactor: 최초 api 응답 데이터 필터링 사용법으로 변경
     - 수정 전: 시/도 select 값을 선택할 때마다 새로운 api 호출

--- src/pages/Myregion.jsx
+++ src/pages/Myregion.jsx
@@ -9,38 +9,48 @@ import Header from '../components/Header'
 function Myregion() {
   const dispatch = useDispatch()
 
+  // 미세먼지 api 호출
   useEffect(() => {
-    dispatch(getAirInfo('서울'))
+    dispatch(getAirInfo())
   }, [])
+
+  // 미세먼지 data 할당
   const { data } = useSelector((state) => state.airInfo)
 
-  let getData = !data ? [] : data
+  const [selectedSido, setSelectedSido] = useState('')
+  const getSidoName = (name) => {
+    setSelectedSido(name)
+  }
 
-  let stationName = getData.map((item, i) => {
-    return item.stationName
-  })
+  let filteredSidoData = data.filter((item) => item.sidoName === selectedSido)
+  let stationNames = filteredSidoData.map((item) => item.stationName)
+  console.log('filter', stationNames)
 
   const [selectedStation, setSelectedStation] = useState('')
   const getStationName = (name) => {
     setSelectedStation(name)
   }
 
-  let filteredStationData = getData.filter(
+  let filteredStationData = data.filter(
     (item) => item.stationName === selectedStation
   )
 
   return (
     <>
       <Header>
-        <Select name={'시/도'} items={sidoNames} dispatchHandler={getAirInfo} />
+        <Select name={'시/도'} items={sidoNames} getValue={getSidoName} />
         <Select
           width={'12.8rem'}
           name={'지역'}
-          items={stationName}
+          items={stationNames}
           getValue={getStationName}
         />
       </Header>
-      <AirInfo data={filteredStationData} />
+      <AirInfo
+        data={filteredStationData}
+        bookmark={null}
+        text={'지역을 선택하세요.'}
+      />
     </>
   )
 }
