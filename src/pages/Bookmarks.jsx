a7fe4d938e81277a6fc0436180eca2ccd0f15a20
Hwa-J <hj1103.code@gmail.com>
2022-08-23 04:47:38 +0900

    style: 구조 변경

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Header from '../components/Header'
+import AirInfo from '../components/AirInfo'
 
 function Bookmark() {
-  return <div>Bookmark</div>
+  return (
+    <>
+      <Header>즐겨찾기 목록</Header>
+      <AirInfo text={'즐겨찾는 지역을 선택하세요.'} />
+    </>
+  )
 }
 
 export default Bookmark
