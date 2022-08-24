b4aebd3ea7fe40b5da46dc927747a47c63f3484f
Hwa-J <hj1103.code@gmail.com>
2022-08-24 22:24:51 +0900

    refactor: 즐겨찾기 목록 렌더링, 즐겨찾기 삭제 기능 적용

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import Header from '../components/Header'
 import AirInfo from '../components/AirInfo'
+import { useDispatch, useSelector } from 'react-redux'
 
 function Bookmark() {
+  const { bookmarkedAirInfo } = useSelector((state) => state.airInfo)
+
+  const REMOVE_BOOKMARKED_AIR_INFO = 'air-info/REMOVE_BOOKMARKED_AIR_INFO'
+
+  const dispatch = useDispatch()
+  const remover = (item) =>
+    dispatch({
+      type: REMOVE_BOOKMARKED_AIR_INFO,
+      payload: item,
+    })
+
   return (
     <>
       <Header>즐겨찾기 목록</Header>
-      <AirInfo text={'즐겨찾는 지역을 선택하세요.'} />
+      <AirInfo
+        text={'즐겨찾기가 없습니다.'}
+        data={bookmarkedAirInfo}
+        star={true}
+        bookmarkRemoveHandler={remover}
+      />
     </>
   )
 }
