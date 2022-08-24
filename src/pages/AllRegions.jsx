9e5dc7c7a9b52d7f1ee06b2b52593299ad17a6f9
Hwa-J <hj1103.code@gmail.com>
2022-08-24 22:21:39 +0900

    refactor: 미세먼지 api 정보 호출 제어 외 2건
    - api 응답 데이터 여부에 따라 api 호출
    - 즐겨찾기 추가/삭제 기능 적용
    - select의 시/도 값에 따라 data값 필터링 및 렌더링

--- src/pages/AllRegions.jsx
+++ src/pages/AllRegions.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getAirInfo } from '~/modules/air-info'
 import AirInfo from '../components/AirInfo'
@@ -11,21 +11,51 @@ function AllRegions() {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(getAirInfo('서울'))
-  }, [dispatch])
+    !data && dispatch(getAirInfo('전국'))
+  }, [])
 
-  let getData = !data ? [] : data
+  const GET_BOOKMARKED_AIR_INFO = 'air-info/GET_BOOKMARKED_AIR_INFO'
+  const REMOVE_BOOKMARKED_AIR_INFO = 'air-info/REMOVE_BOOKMARKED_AIR_INFO'
+
+  const [selectedSido, setSelectedSido] = useState('')
+
+  const getSidoName = (name) => {
+    setSelectedSido(name)
+  }
+  console.log(getSidoName)
+  let filteredSidoData = data
+    ? selectedSido === '전국'
+      ? data
+      : data.filter((item) => item.sidoName === selectedSido)
+    : []
+
+  const bookmarker = (item) =>
+    dispatch({
+      type: GET_BOOKMARKED_AIR_INFO,
+      payload: item,
+    })
+  const remover = (item) =>
+    dispatch({
+      type: REMOVE_BOOKMARKED_AIR_INFO,
+      payload: item,
+    })
 
   return (
     <>
       <Header>
         <Select
           width={'10rem'}
+          name={'전국'}
           items={sidoNames}
-          dispatchHandler={getAirInfo}
+          getValue={getSidoName}
         />
       </Header>
-      <AirInfo data={getData} />
+      <AirInfo
+        data={filteredSidoData}
+        star={true}
+        bookmarkHandler={bookmarker}
+        bookmarkRemoveHandler={remover}
+      />
     </>
   )
 }
